aef0cbe48d1813657905f02b16057c2e8323a961
Devashish Choudhary <devashishchaudhary2908@gmail.com>
2023-07-23 20:30:54 +0530

    Final List

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,10 +3,13 @@ import { Container, Row, Col } from "react-bootstrap";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
 import ProjectCard from "./ProjectCards";
 
-import PrivateChat from "../../Assets/Projects/PrivateChat.png";
+
 import Portfolio from "../../Assets/Projects/Portfolio.png";
 import Ecart from "../../Assets/Projects/ecart.png";
-import ExpenseTracker from "../../Assets/Projects/ExpenseTracker.png";
+import car from "../../Assets/Projects/car.png";
+import FLIGHT from "../../Assets/Projects/FLIGHT.png";
+import tb_real from "../../Assets/Projects/tb_real.png";
+import credit from "../../Assets/Projects/credit.jpg";
 
 import "./project.css";
 
@@ -24,8 +27,8 @@ function Projects() {
               imgPath={Ecart}
               title="E Cart"
               description="This is a fully functional eCommerce website that uses React.js + MUI and CSS in the front end and NodeJs and express js in backend and MongoDb as Database."
-              ghLink="https://github.com/rahuljha4171/E-cart"
-              demoLink="ttps://github.com/rahuljha4171/E-cart"
+              ghLink="https://github.com/DevashishChoudharyA/E-Commerce-Website"
+              demoLink="https://github.com/DevashishChoudharyA/E-Commerce-Website"
             />
           </Col>
 
@@ -40,41 +43,41 @@ function Projects() {
           </Col>
           <Col md={6} lg={4} className="project-card">
             <ProjectCard
-              imgPath={PrivateChat}
-              title="Private Chat"
-              description="A Personal Chat Application to share resources and hangout with friends build with react.js, css, and Firebase. Have features which allows user for realtime messaging, image sharing and search user."
-              ghLink="#"
-              demoLink="https://chat-app-rahuljha4171.vercel.app/"
+              imgPath={tb_real}
+              title="Bangalore House Price Prediction System"
+              description="A Personal Machine Learning based Application based on the problems of Real Estate Sector build for the people who want an approximate idea of the cost of poperty depending upon certain factors. Built with react.js, Flask , Python , and deployed using render. Have features which allows user to check prices by varying various factors."
+              ghLink="https://github.com/DevashishChoudharyA/Car-Price-Prediction"
+              demoLink="https://car-price-prediction-sdpw.onrender.com/"
             />
           </Col>
 
           <Col md={6} lg={4} className="project-card">
             <ProjectCard
-              imgPath={ExpenseTracker}
-              title="Expense Tracker"
-              description="An online expense tracker using React, Node, Express, and MongoDB as the database. It allows you to keep track of your expenses, investments, and savings. All past transactions are listed, and you can also delete them. "
-              ghLink="https://github.com/rahuljha4171/Expense-Tracker"
-              demoLink="https://expense-tracker.rahuljha4171.vercel.app/"
+              imgPath={FLIGHT}
+              title="Flight Price Estimator"
+              description="Flight price Estimator is a web application which can help you plan and save a lot of money by getting the estimate prices of tickets by entering the dates of travel and boarding and destination location. It used HTML , CSS and JavaScript for Front End and Flask for Backend and deployed using Render . "
+              ghLink="https://github.com/DevashishChoudharyA/Flight-Price-Estimator"
+              demoLink="https://flight-price-2zys.onrender.com/"
             />
           </Col>
 
           <Col md={6} lg={4} className="project-card">
             <ProjectCard
-              imgPath={ExpenseTracker}
-              title="Expense Tracker"
-              description="An online expense tracker using React, Node, Express, and MongoDB as the database. It allows you to keep track of your expenses, investments, and savings. All past transactions are listed, and you can also delete them. "
-              ghLink="https://github.com/rahuljha4171/Expense-Tracker"
-              demoLink="https://expense-tracker.rahuljha4171.vercel.app/"
+              imgPath={credit}
+              title="Credit Risk Analysis"
+              description="The credit risk analysis project aimed to assess the creditworthiness and potential risks associated with the loan portfolio of XYZ Financial Institution. Through a comprehensive analysis of borrower data, financial statements, and credit histories, the project aimed to identify potential areas of concern and provide recommendations for effective risk management. "
+              ghLink="https://github.com/DevashishChoudharyA/Credit-Risk-Analysis"
+              demoLink="https://github.com/DevashishChoudharyA/Credit-Risk-Analysis"
             />
           </Col>
 
           <Col md={6} lg={4} className="project-card">
             <ProjectCard
-              imgPath={ExpenseTracker}
+              imgPath={car}
               title="Expense Tracker"
-              description="An online expense tracker using React, Node, Express, and MongoDB as the database. It allows you to keep track of your expenses, investments, and savings. All past transactions are listed, and you can also delete them. "
-              ghLink="https://github.com/rahuljha4171/Expense-Tracker"
-              demoLink="https://expense-tracker.rahuljha4171.vercel.app/"
+              description="Car Price Predictor is a project based on Machine Learning which is a web application that can be used to estimate the current price of a car based on various factors such as number of kilometers driven , acquisition year , etc. This web app's Front End was built using React and JavaScript and the BackEnd was built using Django and deployed using Render. "
+              ghLink="https://github.com/DevashishChoudharyA/Car-Price-Prediction"
+              demoLink="https://car-price-prediction-sdpw.onrender.com/"
             />
           </Col>
           
