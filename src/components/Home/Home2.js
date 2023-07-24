cc25e5c57122e45e701e0a026d3517c5970c2d67
Devashish Choudhary <devashishchaudhary2908@gmail.com>
2023-07-24 13:16:37 +0530

    added link of social profiles

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -21,33 +21,23 @@ function Home2() {
             </h1>
             <p className="home-about-body justify" data-aos="fade-up">
               <br />
-              <div className="text-center justify-center">
-A Software Engineer – an innovative, creative, and resourceful problem solver with a <br />
-passion 
-for building cutting-edge solutions! 🎯 With a love for coding that runs deep <br />in me, I have 
-this uncanny ability to &nbsp;transform&nbsp; complex challenges &nbsp;into elegant &nbsp;and <br /> efficient software masterpieces. 💡
+              <div  style={{textAlign:"justify", width:"90%"}}>
+A Software Engineer – an innovative, creative, and resourceful problem solver with a 
+passion for building cutting-edge solutions! 🎯 With a love for coding that runs deep in me, I have 
+this uncanny ability to transform complex challenges into elegant and  efficient software masterpieces. 💡
 <br /> <br />
-I am passionate about{" "}
-              <i className="primary-header">technologies</i> and love to learn
-              new things.
-            
-             I am more interested <br />in building new
-              <i>
-                <i className="primary-header"> Technologies and Products.</i>
-              </i>
-              
-              I &nbsp;always apply my experience in developing <br />products with{" "}
-              <i className="primary-header">Node.js and Django</i> and Modern&nbsp; Javascript
-              Library&nbsp; and&nbsp; Framework like <br />
-              <i className="primary-header"> React.js.</i>
+I am passionate about technologies and love to learn new things. I am more interested in building new 
+technologies and products. I always apply my experience in developing products with Node.js and Django
+and Modern Javascript Library Framework like React.js.
               <br /><br />
 
 
-But Alongside &nbsp;my&nbsp; brilliant&nbsp; coding&nbsp; prowess, &nbsp;I am an &nbsp;empathetic collaborator and an <br />
-exceptional&nbsp; team player. Working &nbsp;side by side&nbsp; with me &nbsp;guarantees a&nbsp; delightful&nbsp; and  <br />
-inspiring &nbsp;journey&nbsp; for all!&nbsp; 👥 So, if&nbsp; you're&nbsp; seeking a&nbsp;  Software Engineer who not 
-only <br /> dreams big but also turns those dreams into reality with finesse and flair,
-you've found <br /> the one! &nbsp;Together, let's &nbsp;embark &nbsp;on an &nbsp;adventure &nbsp;that will &nbsp;redefine the  boundaries of  <br />innovation and redefine the future! 🚀💻
+But Alongside brilliant coding prowess, I am an empathetic collaborator and an 
+exceptional&nbsp; team player. Working &nbsp;side by side&nbsp; with me &nbsp;guarantees a delightful and 
+inspiring journey for all! 👥 So, if you're seeking a  Software Engineer who not 
+only dreams big but also turns those dreams into reality with finesse and flair,
+you've found the one! Together, let's embark on an adventure that will redefine the  
+boundaries of  innovation and redefine the future! 🚀💻
               
              
               <br />
@@ -56,6 +46,7 @@ you've found <br /> the one! &nbsp;Together, let's &nbsp;embark &nbsp;on an &nbs
             </p>
           </Col>
           <Col md={4} className="myAvtar center">
+            <br /> <br /> <br /> <br /><br />
             <Tilt>
               <img
                 data-aos="fade-left"
@@ -75,7 +66,7 @@ you've found <br /> the one! &nbsp;Together, let's &nbsp;embark &nbsp;on an &nbs
             <ul className="home-about-social-links" data-aos="fade-up">
               <li className="social-icons">
                 <a
-                  href="https://github.com/Rahuljha4171"
+                  href="https://github.com/DevashishChoudharyA"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -84,7 +75,7 @@ you've found <br /> the one! &nbsp;Together, let's &nbsp;embark &nbsp;on an &nbs
                   <AiFillGithub />
                 </a>
               </li>
-              <li className="social-icons">
+              {/* <li className="social-icons">
                 <a
                   href="https://twitter.com/Rahuljha4171"
                   target="_blank"
@@ -94,10 +85,10 @@ you've found <br /> the one! &nbsp;Together, let's &nbsp;embark &nbsp;on an &nbs
                 >
                   <AiOutlineTwitter />
                 </a>
-              </li>
+              </li> */}
               <li className="social-icons">
                 <a
-                  href="mailto:rahuljha4171@outlook.com"
+                  href="mailto:choudharydev836@gmail.com"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -108,7 +99,7 @@ you've found <br /> the one! &nbsp;Together, let's &nbsp;embark &nbsp;on an &nbs
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/Rahuljha4171/"
+                  href="https://www.linkedin.com/in/devashishch/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -119,7 +110,7 @@ you've found <br /> the one! &nbsp;Together, let's &nbsp;embark &nbsp;on an &nbs
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/dead.programmer/"
+                  href="https://www.instagram.com/devashishchoudhary1/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
